d98725bae8f68cc1d617237eda84549eb1bf987f
Hi-Ren11 <hiren.typing@gmil.com>
2023-05-18 11:39:13 -0700

    alx-low_level_programming

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: This uses a puts function to print a program
+ *
+ * Return: always zero on success
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
