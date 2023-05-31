4b2b17f0572aa2f647bbb5b769a4695adee1d35e
Hi-Ren11 <hiren.typing@gmil.com>
2023-05-31 09:19:19 -0700

    Version 1

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string
+ * @str: string to print
+ *
+ * Description: function that prints a string, followed by a new line to stdout
+ */
+
+void _puts(char *str)
+{
+	for (; *str++;)
+	{
+		_putchar(*str);
+	}
+	_putchar('\n');
+}
