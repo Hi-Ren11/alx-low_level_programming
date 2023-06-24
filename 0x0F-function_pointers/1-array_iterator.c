e1496b42a9c9c9479dea5b545f207940745f31f4
Hi-Ren11 <hiren.typing@gmil.com>
2023-06-24 09:48:00 -0700

    version 1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -10,7 +10,18 @@
  *
  * Return: void
  */
+
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
+	size_t i;
 
+	if (array && action)
+	{
+		i = 0;
+		while (i < size)
+		{
+			action(array[i]);
+			i++;
+		}
+	}
 }
