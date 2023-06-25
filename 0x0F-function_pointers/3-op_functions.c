68d76d81c1318a0214f2fcb01448cb2b2185a484
Hi-Ren11 <hiren.typing@gmil.com>
2023-06-25 00:10:59 -0700

    version 2

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -12,7 +12,7 @@
 
 int op_add(int a, int b)
 {
-
+	return (a + b);
 }
 
 
@@ -28,7 +28,7 @@ int op_add(int a, int b)
 
 int op_sub(int a, int b)
 {
-
+	return (a - b);
 }
 
 
@@ -44,7 +44,7 @@ int op_sub(int a, int b)
 
 int op_mul(int a, int b)
 {
-
+	return (a - b);
 }
 
 
@@ -60,7 +60,13 @@ int op_mul(int a, int b)
 
 int op_div(int a, int b)
 {
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
 
+	return (a / b);
 }
 
 /*********** OP_MOD ***********/
@@ -75,5 +81,11 @@ int op_div(int a, int b)
 
 int op_mod(int a, int b)
 {
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
 
+	return (a % b);
 }
