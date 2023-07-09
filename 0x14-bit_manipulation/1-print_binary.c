691d3dee1a3d1e2bee7363349c709c31368bbcc4
Hi-Ren11 <hiren.typing@gmil.com>
2023-07-09 02:06:50 -0700

    corrected

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -2,31 +2,34 @@
 #include "main.h"
 
 /**
- * print_binary : print binary representation of a number
+ * print_binary - print binary representation of a number
  * @n: decimal number to print as binary
  */
+
 void print_binary(unsigned long int n)
 {
-        unsigned long int temp;
-        int shifts;
+	unsigned long int temp;
+	int shifts;
 
-        if (n == 0)
-        {
-                printf("0");
-                return;
-        }
+	if (n == 0)
+	{
+		printf("0");
+		return;
+	}
 
-        for (temp = n, shifts = 0; (temp >>= 1) > 0; shifts++)
-                ;
+	for (temp = n, shifts = 0; (temp >>= 1) > 0; shifts++)
+		;
 
-        for (; shifts >= 0; shifts--)
-        {
-                if ((n >> shifts) & 1)
-                        printf("1");
-                else
-                        printf("0");
-        }
+	for (; shifts >= 0; shifts--)
+	{
+		if ((n >> shifts) & 1)
+			printf("1");
+		else
+			printf("0");
+	}
 }
+
+
 /*
  * _power - calculate (base and power)
  * @base - base of the exponet
@@ -36,11 +39,11 @@ void print_binary(unsigned long int n)
 
 unsigned long int _power(unsigned int base, unsigned int pow)
 {
-        unsigned long int num;
-        unsigned int i;
+	unsigned long int num;
+	unsigned int i;
 
-        num = 1;
-        for (i = 1; i <= pow; i++)
-                num *= base;
-        return (num);
+	num = 1;
+	for (i = 1; i <= pow; i++)
+		num *= base;
+	return (num);
 }
